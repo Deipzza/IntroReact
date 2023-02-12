a3b1a7f557375de4ea8e4ce09a6aa8d118002bb3
David Pérez <dperezz@unal.edu.co>
2023-02-11 22:16:54 -0500

    Day 5

--- react-basico/src/App.js
+++ react-basico/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Componente from './components/Componente';
+import Propiedades from './components/Propiedades';
 
 const seasons = ["spring", "autumn", "winter", "summer"];
 const auth = true;
@@ -13,30 +14,46 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          {auth ? "The user is authenticated" : "The user is not authenticated"}
-        </p>
-        <ul>
-          {seasons.map((e, index) => (<li key={index}>{e}</li>))}
-        </ul>
-        {/* <audio src="uwu.mp3"></audio> */}
-        {/* <button onClick={document.querySelector("audio")}>Sorpresa</button> */}
+        <section>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <p>
+            {auth ? "The user is authenticated" : "The user is not authenticated"}
+          </p>
+          <ul>
+            {seasons.map((e, index) => (<li key={index}>{e}</li>))}
+          </ul>
+          {/* <audio src="uwu.mp3"></audio> */}
+          {/* <button onClick={document.querySelector("audio")}>Sorpresa</button> */}
+        </section>
+        <section>
+          <Componente msg="Hola desde una prop" />
+          <hr/>
+          <Propiedades 
+            cadena="cadena de texto" 
+            numero={5} 
+            booleano={true} 
+            arreglo={[1, 2, 3]}
+            objeto={{
+              nombre: "John",
+              correo: "john@correo.com",
+            }}
+            elementoReact={<p>Elemento React</p>}
+            funcion={num => num * num}
+            componenteReact={<Componente msg="Soy un componente"/>}
+          />
+        </section>
       </header>
-      <section>
-        <Componente msg="Hola desde una prop" />
-      </section>
     </div>
   );
 }
