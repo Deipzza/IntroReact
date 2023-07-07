d34c9f2cf22efeaca05edf5dec58284b48742662
David Pérez <dperezz@unal.edu.co>
2023-07-07 10:39:30 -0500

    Videos 25 & 26

---
+++ react-css-frameworks/src/app/page.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Bootstrap from '@/Components/Bootstrap';
+import Bulma from '@/Components/Bulma';
+// import ReactBootstrap from '@/Components/ReactBootstrap';
+import MaterialUI from '@/Components/MaterialUI';
+
+export default function Home() {
+  return (
+    <>
+      <h1>Frameworks CSS con React</h1>
+      {/* <Bootstrap /> */}
+      {/* <Bulma /> */}
+      {/* <ReactBootstrap /> */}
+      <MaterialUI />
+    </>
+  )
+}
