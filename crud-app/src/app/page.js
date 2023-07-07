e90b9accc946cbc6336cf0a9f4de6c8ea5ad40bd
David Pérez <dperezz@unal.edu.co>
2023-07-07 18:11:55 -0500

    Videos: 32 & 33: Start of CRUD API

--- crud-app/src/app/page.js
+++ crud-app/src/app/page.js
@@ -2,11 +2,14 @@
 // import styles from './page.module.css'
 
 import CrudApp from "@/components/CrudApp";
+import CrudApi from "@/components/CrudApi";
 
 export default function Home() {
   return (
     <>
       <h1>Ejercicios con React</h1>
+      <CrudApi />
+      <hr />
       <CrudApp />
     </>
   )
